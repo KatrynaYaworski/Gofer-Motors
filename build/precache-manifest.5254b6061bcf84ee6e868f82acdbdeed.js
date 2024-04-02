0c198061e62f39caa173f1bbb6034d2b88fe6079
Katryna Yaworski <Katrynayaworski@gmail.com>
2024-04-01 21:11:52 -0500

    remove build from gitignore

---
+++ build/precache-manifest.5254b6061bcf84ee6e868f82acdbdeed.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0448f72b573e72f61f50a61c71f871e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91f6c60f5b0001ec094c",
+    "url": "/static/css/main.d14d5538.chunk.css"
+  },
+  {
+    "revision": "a26269bc68bfe1e7cab1",
+    "url": "/static/js/2.019189db.chunk.js"
+  },
+  {
+    "revision": "2c7548cfd46fb98165a490fbda8c9f94",
+    "url": "/static/js/2.019189db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f6c60f5b0001ec094c",
+    "url": "/static/js/main.1c9052e4.chunk.js"
+  },
+  {
+    "revision": "e360097aa9acd2f4a83c",
+    "url": "/static/js/runtime-main.c86a5587.js"
+  },
+  {
+    "revision": "9db0a78dfead8422f07e0e7a915e4d4b",
+    "url": "/static/media/Arial top view of used cars1.9db0a78d.png"
+  },
+  {
+    "revision": "4c5046023b266d78f695468f35f523a4",
+    "url": "/static/media/Hero-img.4c504602.jpg"
+  },
+  {
+    "revision": "7de6a02bc6ba760b79a95e96bfde4f2d",
+    "url": "/static/media/aboutus.7de6a02b.jpg"
+  },
+  {
+    "revision": "b20a6700363515cae992781229c7a47d",
+    "url": "/static/media/businessman-showing-key.b20a6700.jpg"
+  },
+  {
+    "revision": "b437c65e6d2e838d43a042b74cce09e3",
+    "url": "/static/media/coming_soon.b437c65e.jpeg"
+  },
+  {
+    "revision": "f84b8d34aa50780bb652eb381c61f3ad",
+    "url": "/static/media/customer-service-woman-working-on-a-phone-call-2022-09-08-19-20-14-utc.f84b8d34.jpg"
+  },
+  {
+    "revision": "1a415da0a7cef04fb61a2ed87da2e52e",
+    "url": "/static/media/customer-shaking-hands.1a415da0.jpg"
+  },
+  {
+    "revision": "fbe621166b7bd7b8ace52f738aa686eb",
+    "url": "/static/media/gofer_logo.fbe62116.png"
+  },
+  {
+    "revision": "cb8f6bfe3d9989e764a83ec935fe60b1",
+    "url": "/static/media/joyful-talk-of-the-woman-and-car-showroom-manager-2023-11-27-05-33-28-utc.cb8f6bfe.jpg"
+  },
+  {
+    "revision": "e8bfa8ee87e9c68047156e00ee6b4196",
+    "url": "/static/media/sold.e8bfa8ee.png"
+  },
+  {
+    "revision": "c3bdaa6f8cd04fa9105e45efad168ef5",
+    "url": "/static/media/standing-by-car-talking.c3bdaa6f.jpg"
+  },
+  {
+    "revision": "4a0abd090e89d8aaa0649c54de121bc3",
+    "url": "/static/media/steering wheel.4a0abd09.jpeg"
+  },
+  {
+    "revision": "dc3bdd80cb847e2971cf908d3e74eb0d",
+    "url": "/static/media/test_drive2.dc3bdd80.jpg"
+  }
+]);
